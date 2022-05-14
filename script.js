1257369ab8450401939c92992a7dbcbbcf20f25d
Sahil bisht <58036566+codemaniac-sahil@users.noreply.github.com>
2022-05-14 22:49:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // https://newsapi.org/v2/top-headlines?country=in&apiKey=7ccf3b8406a94d028b1cd059f0c6d015
 async function getgeolocation(){
     
-    let url='http://ip-api.com/json/';
+    let url='https://ipapi.co/json';
     let response=await fetch(url)
     let data = await response.json()
-    current_location=data.countryCode
-    document.getElementById("country").innerHTML=data.country
+    current_location=data.country_code
+    document.getElementById("country").innerHTML=data.country_name
     async function getnews(){
         url1='https://newsapi.org/v2/top-headlines?country='
         url2=current_location+'&apiKey=7ccf3b8406a94d028b1cd059f0c6d015'
