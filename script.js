960d6628a32e42b8116416ba468852c86ddc53a7
Sahil bisht <58036566+codemaniac-sahil@users.noreply.github.com>
2022-05-15 13:45:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// https://newsapi.org/v2/top-headlines?country=in&apiKey=7ccf3b8406a94d028b1cd059f0c6d015
+// https://gnews.io/api/v4/top-headlines?token=a175ac9bf19a19e7e95f50136ebffcbb&country=in&lang=en
 async function getgeolocation(){
     
     let url='https://ipapi.co/json';
     let response=await fetch(url)
     let data = await response.json()
     current_location=data.country_code
+    current_location
     document.getElementById("country").innerHTML=data.country_name
     async function getnews(){
-        url1='https://newsapi.org/v2/top-headlines?country='
-        url2=current_location+'&apiKey=7ccf3b8406a94d028b1cd059f0c6d015'
+        url1='https://gnews.io/api/v4/top-headlines?token=a175ac9bf19a19e7e95f50136ebffcbb&country='
+        url2=current_location.toLowerCase()+'&lang=en'
         full_url=url1+url2;
         console.log(full_url)
         let response1=await fetch(full_url);
         let data1=await response1.json()
         for(var i=0;i<data1.articles.length;i++){
-            document.getElementById("news").innerHTML+=`<div class='img'><img src=${data1.articles[i].urlToImage}>
+            document.getElementById("news").innerHTML+=`<div class='img'><img src=${data1.articles[i].image}>
             </div>
             <div class='news'><p>${data1.articles[i].title} <a href='${data1.articles[i].url}'> Read full article</p>
             
