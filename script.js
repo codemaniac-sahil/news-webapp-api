14e4bbe951bb36a67619de052a011c2748fc2872
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2022-10-04 02:51:09 -0300

    Fixed no news found bug (#18)
    The filter with en as lang was making 0 news as result so i make it filter by the country langs.

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ async function getgeolocation(){
     let url='https://ipapi.co/json';
     let response=await fetch(url)
     let data = await response.json()
-    current_location=data.country_code
-    current_location
-    document.getElementById("country").innerHTML=data.country_name
+    var current_location = data.country_code
+    var current__lang = data.languages;
+    document.getElementById("country").innerHTML = data.country_name;
     async function getnews(){
-        url1='https://gnews.io/api/v4/top-headlines?token=a175ac9bf19a19e7e95f50136ebffcbb&country='
-        url2=current_location.toLowerCase()+'&lang=en'
-        full_url=url1+url2;
-        let response1=await fetch(full_url);
-        let data1=await response1.json()
+        let full_url ='https://gnews.io/api/v4/top-headlines?token=a175ac9bf19a19e7e95f50136ebffcbb&country=' + current_location.toLowerCase()+ '&lang=' + current__lang.toLowerCase();
+        console.log(full_url)
+        let response1 = await fetch(full_url);
+        let data1 = await response1.json()
         for(var i=0;i<data1.articles.length;i++){
             document.getElementById("news").innerHTML+=`     <div class='news'><p>${data1.articles[i].title} <a class="read" href='${data1.articles[i].url}'> Read full article</p>
             <div class='img'><img src=${data1.articles[i].image}>
